c54cc1d9de51060c3316a5492417bb11ad5aeda1
aruyamam <aruyamam@gmail.com>
2018-03-16 20:38:39 +0900

    Section5 Lifecycle Methods & Saving and Loading Options Data

--- src/app.js
+++ src/app.js
@@ -10,6 +10,31 @@ class IndecisionApp extends React.Component {
     };
   }
 
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    }
+    catch (e) {
+      
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
   handleDeleteOptions() {
     this.setState(() => ({ options: [] }));
   }
@@ -96,6 +121,7 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && <p>Please add and option to get started!</p>}
       {
         props.options.map((option) => (
           <Option
@@ -140,6 +166,10 @@ class AddOption extends React.Component {
     const error = this.props.handleAddOption(option);
 
     this.setState(() =>  { error });
+
+    if (!error) {
+      e.target.elements.option.value = '';
+    }
   }
 
   render() {

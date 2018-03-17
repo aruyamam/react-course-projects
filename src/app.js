803aa1078eed9b08c32c4ee01e9d6aec2109bfcb
aruyamam <aruyamam@gmail.com>
2018-03-17 21:16:49 +0900

    Section5 One Compenent per File

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
-// import './utils';
-import { add } from './utils';
-import { isAdult, canDrink } from './person';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './compenents/IndecisionApp';
 
-console.log('app.js is running');
-// console.log(square(4));
-console.log(add(100, 23));
-console.log(isAdult(18));
-console.log(canDrink(18));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
